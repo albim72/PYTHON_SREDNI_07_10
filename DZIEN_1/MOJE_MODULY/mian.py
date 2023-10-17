399d9b075570480c6f41315c40338b65f86e6cfb
albim72 <marcin.albiniak72@gmail.com>
2023-10-17 15:52:24 +0200

    Update mian.py

--- DZIEN_1/MOJE_MODULY/mian.py
+++ DZIEN_1/MOJE_MODULY/mian.py
@@ -4,6 +4,8 @@ from dane import nrfilii as nf
 from dane import book as bk
 from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
 
+from klasa.bklasa import CDane
+
 print("____________ dane _________________")
 print(nf)
 print(bk)
@@ -11,3 +13,8 @@ print(bk)
 print("____________ dane przez funkcje _________________")
 czytaj_liste(nf)
 czytaj_slownik(bk)
+
+print("____________ dane przez klasę _________________")
+cd = CDane(nf,bk)
+cd.rdl()
+cd.rdd()
