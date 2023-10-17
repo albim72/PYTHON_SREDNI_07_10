e9c63fea81aec79b73b29cde31d3ad35bba31352
albim72 <marcin.albiniak72@gmail.com>
2023-10-17 11:56:42 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -15,3 +15,14 @@ print(policz(4,7,2,6))
 print(policz(6.7,3,6.88,2))
 print(policz(6.7,3,6.88))
 print(n)
+
+#przykład 2
+
+def calc(x,l,k=4,n=3.33333,t=0):
+    return l+x**3/math.sqrt(k-1)*n + t
+
+
+print(calc(2,1,2,1))
+print(calc(2,1,2))
+print(calc(2,1))
+print(calc(3,2,n=10,t=2))
