f53babaf3e3fc0abfaf9e60afe30a6ec8bf6d69b
albim72 <marcin.albiniak72@gmail.com>
2023-10-17 11:19:43 +0200

    Update kolekcje.py

--- DZIEN_1/kolekcje.py
+++ DZIEN_1/kolekcje.py
@@ -61,3 +61,54 @@ print(f'lista t1 po zmianie: {t1}')
 print(f'lista t2 po zmianie: {t2}')
 print(f'lista t3 po zmianie: {t3}')
 
+#krotka -> lista niemutowalna
+miasta = ("Kraków","Tarnów","Gdańsk","Wrocław","Kraków","Warszawa",434)
+
+print(miasta)
+print(miasta.count("Kraków"))
+print(miasta.count(434))
+print(len(miasta))
+print(miasta[0:3])
+
+#zbiór -> set
+
+kolory = {"zielony","czerwony","niebieski","biały","czarny","brązowy","biały"}
+print(kolory)
+print(kolory)
+print(kolory)
+
+setcity = set(miasta)
+miasta = tuple(setcity)
+print(miasta)
+
+kolory.remove("brązowy")
+print(kolory)
+
+kolory.discard("biały")
+print(kolory)
+
+#słownik -> dict
+osoba = {
+    "imię":"Jan",
+    "nazwisko":"Kowal",
+    "wiek":56,
+    "miasto":"Rzeszów",
+    "miasto urodzenia":"Rzeszów",
+    "nazwisko":"Kobus"
+}
+
+print(osoba)
+print(type(osoba))
+print(osoba["imię"])
+osoba["miasto"] = "Sanok"
+print(osoba)
+osoba["zawód"] = "Programista"
+print(osoba)
+
+print(osoba.keys())
+print(osoba.values())
+print(osoba.items())
+
+print("_____________ Items ______________")
+for x,y in osoba.items():
+    print(f"{x} : {y}")
