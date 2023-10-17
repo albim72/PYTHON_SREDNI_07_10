8f2d8a0678c4f19cb1b61599a8f530a3d8de572d
albim72 <marcin.albiniak72@gmail.com>
2023-10-17 15:47:17 +0200

    Create mian.py

---
+++ DZIEN_1/MOJE_MODULY/mian.py
@@ -0,0 +1,13 @@
+#import dane
+#import dane as dn
+from dane import nrfilii as nf
+from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+
+print("____________ dane _________________")
+print(nf)
+print(bk)
+
+print("____________ dane przez funkcje _________________")
+czytaj_liste(nf)
+czytaj_slownik(bk)
