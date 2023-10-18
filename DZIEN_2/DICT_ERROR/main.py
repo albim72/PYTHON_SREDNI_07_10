d10b803b6e564d49db54b09a2ac73c489667b663
albim72 <marcin.albiniak72@gmail.com>
2023-10-18 09:56:59 +0200

    Update main.py

--- DZIEN_2/DICT_ERROR/main.py
+++ DZIEN_2/DICT_ERROR/main.py
@@ -5,7 +5,7 @@ from derrors import KeyValueConstructError, IntFloatValueError
 
 class CustomInFloatDict(dict):
     empty_dict = {}
-    
+
     def __init__(self, key=None, value = None):
         if key is None or value is None:
             self.get_dict()
@@ -17,11 +17,24 @@ class CustomInFloatDict(dict):
                 if not isinstance(val,(int,float)):
                     raise IntFloatValueError(val)
                 dict.__setitem__(self,k,val)
-          
+
     def get_dict(self):
         return self.empty_dict
-    
+
     def __setitem__(self, key, value):
         if not isinstance(value, (int, float)):
             raise IntFloatValueError(value)
         return dict.__setitem__(self,key,value)
+
+test_1 = CustomInFloatDict()
+print(test_1)
+print(type(test_1))
+
+# test_2 = CustomInFloatDict({'a','b'},[4,7])
+# print(test_2)
+
+# test_3 = CustomInFloatDict(('x','y','z'),(10,"twenty",30))
+# print(test_3)
+
+test_4= CustomInFloatDict(('x','y','z'),(10,20,30))
+print(test_4)
