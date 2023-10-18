9da378cb58e0175b5bbd6ddb08b1a28d0c5ef161
albim72 <marcin.albiniak72@gmail.com>
2023-10-18 15:07:47 +0200

    Update agregacja.py

--- DZIEN_2/agregacja.py
+++ DZIEN_2/agregacja.py
@@ -2,9 +2,24 @@ class Departament:
     def __init__(self,name,employees):
         self.name = name
         self.employees = employees
-        
+
 class Employee:
     def __init__(self,name,department):
         self.name = name
         self.department = department
-            
+
+
+
+department = Departament("IT",[])
+employee1 = Employee("Janek Kot",department)
+employee2 = Employee("Anna Nowak",department)
+employee3 = Employee("Jarek Kos",department)
+department.employees.append(employee1)
+department.employees.append(employee2)
+department.employees.append(employee3)
+
+print(department.name)
+print(len(department.employees))
+print(department.employees[0].name)
+print(department.employees[1].name)
+print(department.employees[2].name)
