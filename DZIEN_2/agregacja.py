756538d2abacbc84d831377a7fff117f31883557
albim72 <marcin.albiniak72@gmail.com>
2023-10-18 15:01:00 +0200

    Create agregacja.py

---
+++ DZIEN_2/agregacja.py
@@ -0,0 +1,10 @@
+class Departament:
+    def __init__(self,name,employees):
+        self.name = name
+        self.employees = employees
+        
+class Employee:
+    def __init__(self,name,department):
+        self.name = name
+        self.department = department
+            
