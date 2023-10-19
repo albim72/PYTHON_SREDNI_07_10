d8039092c357180c017bfe0c46fdb35dfe23dc3e
albim72 <marcin.albiniak72@gmail.com>
2023-10-19 13:58:58 +0200

    Update main.py

--- DZIEN_3/PROPERTYRESISTANCE/main.py
+++ DZIEN_3/PROPERTYRESISTANCE/main.py
@@ -1,5 +1,6 @@
 from oldresistance import OldResistor
 from resistor import Resistor
+from voltage import VoltageResistance
 
 r0 = OldResistor(10.2E2)
 print(f"______________ klasa {r0.__class__.__name__} ________________")
@@ -18,3 +19,11 @@ print(f'układ eleketryczny:\n'
       f'oporność: {r1.ohms} omów\n'
       f'napięcie prądu: {r1.voltage} V\n'
       f'natężenie prądu: {r1.current} A\n')
+
+r2 = VoltageResistance(1.2E2)
+print(f"______________ klasa {r2.__class__.__name__} ________________")
+r1 = Resistor(45.99E2)
+print(f"______________ klasa {r1.__class__.__name__} ________________")
+r2.voltage = 49
+print(f'napięcie prądu: {r2.voltage} V')
+print(f'natężenie prądu: {r2.current} A')
