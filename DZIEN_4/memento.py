6f4a0b2a0b706f7b53bd896189755f8a06e74ef8
albim72 <marcin.albiniak72@gmail.com>
2023-10-20 14:53:11 +0200

    Update memento.py

--- DZIEN_4/memento.py
+++ DZIEN_4/memento.py
@@ -2,19 +2,26 @@ class Memento:
     def __init__(self,file,content):
         self.file = file
         self.content = content
-        
-        
+
+
 class FileWriterUtility:
     def __init__(self,file):
         self.file = file
         self.content = ""
-        
+
     def write(self,string):
         self.content += string
-        
+
     def save(self):
         return Memento(self.file,self.content)
-    
+
     def undo(self,memento):
         self.content = memento.content
+
+
+class FileWriteCretaker:
+    def save(self,writer):
+        self.obj = writer.save()
         
+    def undo(self,writer):
+        writer.undo(self.obj)
