903533e9d1a1e47a846edc825d8215c84dd66908
albim72 <marcin.albiniak72@gmail.com>
2023-10-20 14:58:50 +0200

    Update memento.py

--- DZIEN_4/memento.py
+++ DZIEN_4/memento.py
@@ -22,6 +22,21 @@ class FileWriterUtility:
 class FileWriteCretaker:
     def save(self,writer):
         self.obj = writer.save()
-        
+
     def undo(self,writer):
         writer.undo(self.obj)
+
+if __name__ == '__main__':
+    caretaaker = FileWriteCretaker()
+    writer = FileWriterUtility("info.txt")
+    writer.write("Pierwsza odsłona pliku info...\n")
+    print(writer.content +"\n")
+    caretaaker.save(writer)
+
+    print("_"*50)
+    writer.write("druga wersja zawartości...")
+    print(writer.content + "\n")
+
+    print("_"*50)
+    caretaaker.undo(writer)
+    print(writer.content + "\n")
