3c4b5afb7d50891b5e4d13a1dc33e22677ca453d
albim72 <marcin.albiniak72@gmail.com>
2023-10-20 10:17:56 +0200

    Update priorytety_sort.py

--- DZIEN_4/priorytety_sort.py
+++ DZIEN_4/priorytety_sort.py
@@ -1,2 +1,12 @@
 numbers = [8,3,1,2,5,4,7,6,7,2,5,8,6,2]
 group = {2,3,5,7}
+
+def sort_prior(values,group):
+    def helper(x):
+        if x in group:
+            return (0,x)
+        return (1,x)
+    values.sort(key=helper)
+
+sort_prior(numbers,group)
+print(numbers)
