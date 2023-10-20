71df692cd45c4a9e531bfc7c44edb098a6e19516
albim72 <marcin.albiniak72@gmail.com>
2023-10-20 11:26:00 +0200

    Update itertools.py

--- DZIEN_4/itertools.py
+++ DZIEN_4/itertools.py
@@ -1,7 +1,9 @@
 import math
 import operator
 import time
+import itertools
 
+#przykład 1
 L1 = [1,2,3]
 L2 = [2,3,4]
 
@@ -17,3 +19,13 @@ print(f"czas wykonania funkcji: {t2-t1} s")
 #a,b,c = math.prod(L1,L2)
 for i in range(3):
     print(L1[i]*L2[i],end = " ")
+
+#przykład 2
+print()
+print("*"*50)
+
+for i in itertools.count(5,5):
+    if i==135:
+        break
+    else:
+        print(i, end = " ")
